5bc9a9047599933905271dabdf44e1a94a46d2f6
liam.wes <57922825+LiamEinspahr@users.noreply.github.com>
2023-02-17 16:01:17 -0600

    Added headercontainer

--- crest/src/app/app.module.ts
+++ crest/src/app/app.module.ts
@@ -1,39 +1,65 @@
+//Default modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home';
 import { AppComponent } from './app.component';
-import { SearchBarComponent } from './searchbar/searchbar.component';
-import { UnitsComponent } from './units/units.component';
-import { EquipmentComponent } from './equipment/equipment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ɵAnimationGroupPlayer } from '@angular/animations';
-import { WelcomescreenComponent } from './welcomescreen/welcomescreen.component';
-import { BottomNavigationComponent } from './bottomnavigation/bottomnavigation.component';
 
-import {MatButtonModule} from '@angular/material/button';
-import {TextFieldModule} from '@angular/cdk/text-field';
+
+
+//Custom page componenets
+import { HomeComponent } from './home';
+import { UnitsComponent } from './units/units.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 import { CrestsComponent } from './crests/crests.component';
+
+//Custom functional components
+import { BottomNavigationComponent } from './bottomnavigation/bottomnavigation.component';
+import { WelcomescreenComponent } from './welcomescreen/welcomescreen.component';
+import { SearchBarComponent } from './searchbar/searchbar.component';
+import { HeaderContainerComponent } from './headercontainer/headercontainer.component';
+
+//Angular Material Modules
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    //custom page components
     HomeComponent,
-    SearchBarComponent,
     UnitsComponent,
     EquipmentComponent,
-    WelcomescreenComponent,
-    BottomNavigationComponent,
     CrestsComponent,
-    
+    //custom functional components
+    BottomNavigationComponent,
+    WelcomescreenComponent,
+    SearchBarComponent,
+    HeaderContainerComponent,
   ],
   imports: [
+    MatInputModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    //imported modules
+    MatFormFieldModule,
     MatButtonModule,
-    TextFieldModule
+    TextFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSelectModule,
   ],
   exports: [
   ],
---
+++ crest/src/app/headercontainer/headercontainer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headercontainer',
+  templateUrl: './headercontainer.component.html',
+  styleUrls: ['./headercontainer.component.scss']
+})
+export class HeaderContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
